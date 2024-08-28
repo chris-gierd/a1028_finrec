33db64bf1e171b8430cd5de6e4a82123102fbe6f
chris-gierd <164418957+chris-gierd@users.noreply.github.com>
2024-08-28 16:17:59 -0700

    Update qa_ebay_orders_manual.sql

--- qa_ebay_orders_manual.sql
+++ qa_ebay_orders_manual.sql
@@ -1,7 +1,9 @@
+CREATE OR REPLACE TEMP TABLE ebay_orders_recon AS
+
 Select transaction_creation_date
 ,type
 ,et.order_number
-,pr.order_number
+--,pr.order_number 
 ,et.payout_id
 ,et.transaction_id
 ,et.item_subtotal
@@ -15,4 +17,13 @@ on et.order_number = pr.order_number
 where 1=1
 AND et.payout_id = '6472238658'
 AND et.type = 'Order'
-AND pr.order_number is null
+AND pr.order_number is null;
+
+
+Select * from `gierd-finrec.a1028_dev.finrec_output` where order_number in (Select order_number from ebay_orders_recon);
+
+Select received_date, reference_number, channel_sku, source_table 
+from client-datawarehouse.a1028_mart.v_linn_orders_current 
+where reference_number in (Select order_number from ebay_orders_recon);
+
+Select gross_transaction_amount, unit
