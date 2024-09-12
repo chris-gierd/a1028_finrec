d4104c9f221f090ec4c62efad15a45a86eaa72ac
chris-gierd <164418957+chris-gierd@users.noreply.github.com>
2024-09-12 10:02:05 -0700

    Update shipping_costs.sql

--- shipping_costs.sql
+++ shipping_costs.sql
@@ -7,6 +7,12 @@ where Shipment_Date >= '2024-08-01'
 group by Shipment_Date, invoice_number, Invoice_Date
 order by shipment_date asc;
 
+Select Shipment_Date, count(net_charge_amount) AS shipments, sum(Net_Charge_Amount), Invoice_Number, Invoice_Date
+FROM `client-datawarehouse.a1028_raw.fedex_data` 
+where Shipment_Date >= '2024-08-01'
+group by Shipment_Date, invoice_number, Invoice_Date
+order by shipment_date asc;
+
 select t.order_id
 ,t.reference_number
 ,t.tracking_number
@@ -15,8 +21,32 @@ select t.order_id
 ,t.Processed_date_pst
 ,s.Shipment_Date
 ,s.Net_Charge_Amount
+,o.tracking_quantity
+,(s.Net_Charge_Amount / o.tracking_quantity) as item_net_ship
+,r.ship_and_handle_gross
+,t.item_title
+,t.sku
 FROM `client-datawarehouse.a1028_mart.v_linn_orders_current` t
   left join `client-datawarehouse.a1028_raw.fedex_data` s
     on t.tracking_number = s.Express_or_Ground_Tracking_ID
-WHERE t.Processed_date_pst >= '2024-08-01' 
-    and t.Processed_date_pst <= '2024-08-30';
+  left join `gierd-finrec.a1028_dev.sales_report` r on t.order_id = r.order_id
+  inner join (
+    SELECT order_id, tracking_number, sum(quantity) tracking_quantity
+    FROM `client-datawarehouse.a1028_mart.v_linn_orders_current`
+    where tracking_number is not null
+    group by order_id, tracking_number 
+  ) o on o.order_id = t.order_id
+WHERE 1=1
+    and t.Processed_date_pst >= '2024-08-01' 
+    and t.Processed_date_pst <= '2024-08-30'
+    and s.Express_or_Ground_Tracking_ID is not null;
+--3090 shipped order-lines in this time period
+--1605 hits on tracking number
+
+--Select * from `client-datawarehouse.a1028_mart.v_linn_orders_current` where order_id = '101955';
+
+--GET order-grain quantity by tracking number
+SELECT order_id, tracking_number, sum(quantity)
+FROM `client-datawarehouse.a1028_mart.v_linn_orders_current`
+where tracking_number is not null
+group by order_id, tracking_number
