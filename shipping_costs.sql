7e0fe8253ff6d505769fb8ff46e9027505d72169
chris-gierd <164418957+chris-gierd@users.noreply.github.com>
2024-09-12 08:56:34 -0700

    Update shipping_costs.sql

--- shipping_costs.sql
+++ shipping_costs.sql
@@ -1,9 +1,11 @@
 --Size up dates we are trying to cover
 Select min(order_date), max(order_date) from gierd-finrec.a1028_dev.sales_report;
 
-Select Shipment_Date, sum(Net_Charge_Amount), Invoice_Number, Invoice_Date
+Select Shipment_Date, count(net_charge_amount) AS shipments, sum(Net_Charge_Amount), Invoice_Number, Invoice_Date
 FROM `client-datawarehouse.a1028_raw.fedex_data` 
-where Shipment_Date >= '2024-08-01';
+where Shipment_Date >= '2024-08-01'
+group by Shipment_Date, invoice_number, Invoice_Date
+order by shipment_date asc;
 
 select t.order_id
 ,t.reference_number
@@ -15,6 +17,6 @@ select t.order_id
 ,s.Net_Charge_Amount
 FROM `client-datawarehouse.a1028_mart.v_linn_orders_current` t
   left join `client-datawarehouse.a1028_raw.fedex_data` s
-    on t.tracking_number = s.Express_or_Ground_Tracking_ID;
-  
-
+    on t.tracking_number = s.Express_or_Ground_Tracking_ID
+WHERE t.Processed_date_pst >= '2024-08-01' 
+    and t.Processed_date_pst <= '2024-08-30';
